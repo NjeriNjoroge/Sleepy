38b5455241178c3474eeba3812f726532be13d89
NjeriNjoroge <grace.njoroge@incentro.com>
2019-05-15 10:45:49 +0300

    find bigger images

--- sleepy/Views/WaterCollectionViewCell.swift
+++ sleepy/Views/WaterCollectionViewCell.swift
@@ -10,12 +10,39 @@ import UIKit
 
 class WaterCollectionViewCell: UICollectionViewCell {
   
+  var cloudImageView: UIImageView = {
+    let img = UIImageView()
+    img.translatesAutoresizingMaskIntoConstraints = false
+    img.contentMode = .scaleAspectFit
+    img.image = UIImage(named: "heavyRain")
+    return img
+  }()
+  
+  var volumeSlider: UISlider = {
+    let slider = UISlider()
+    slider.translatesAutoresizingMaskIntoConstraints = false
+    slider.tintColor = .cyan
+    return slider
+  }()
+  
   override init(frame: CGRect) {
     super.init(frame: frame)
     setupCells()
   }
   
   fileprivate func setupCells() {
+    addSubview(cloudImageView)
+    addSubview(volumeSlider)
+    
+    NSLayoutConstraint.activate([
+      cloudImageView.topAnchor.constraint(equalTo: topAnchor, constant: 12),
+      cloudImageView.widthAnchor.constraint(equalTo: widthAnchor)
+      ])
+    
+    NSLayoutConstraint.activate([
+      volumeSlider.topAnchor.constraint(equalTo: cloudImageView.bottomAnchor, constant: 12),
+      volumeSlider.widthAnchor.constraint(equalTo: widthAnchor)
+      ])
     
   }
   
